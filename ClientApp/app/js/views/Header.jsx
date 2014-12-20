d724b1c3d885179e8ed6c6206a816e5080a33a0d
Andrew <chenpoanandrew@gmail.com>
2014-12-21 05:38:23 +0800

    嘗試修正header

--- ClientApp/app/js/views/Header.jsx
+++ ClientApp/app/js/views/Header.jsx
@@ -13,21 +13,20 @@ var Header = React.createClass({
 
     return (
         <div className="bar">
-        <h3 className="webSiteTitle">Location Based Reddit</h3>
+            <h3 className="webSiteTitle">Location Based Reddit</h3>
 
-        <div className="info">
-            <img className="userImg" src="assets/img/userHeadImg.jpg" alt="userImg" />
-            <h3 className="userName">陳柏安</h3>
-            <h5 className="signIn">log In</h5>
-            <h5 className="signUp">Sign Up</h5>
+            <div className="info">
+                <img className="userImg" src="assets/img/userHeadImg.jpg" alt="userImg" />
+                <h3 className="userName">陳柏安</h3>
+                <h5 className="signIn">log In</h5>
+                <h5 className="signUp">Sign Up</h5>
+            </div>
         </div>
-    </div>
         
-        
-       <div className="header">
+        <div className="header">
             <h1 className="header-name"> Location Based Reddit</h1>
-        <p className="header-by">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;by Team PAMM</p>
-       </div>
+            <p className="header-by">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;by Team PAMM</p>
+        </div>
     );
 
   },
