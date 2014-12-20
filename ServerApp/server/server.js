5249903891058d40a2f9f021d51b5067322c400b
Mickey <mickey@lt>
2014-12-20 19:02:58 +0800

    updated ServerApp server.js

--- ServerApp/server/server.js
+++ ServerApp/server/server.js
@@ -1,11 +1,21 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var path = require('path');
 
 var app = module.exports = loopback();
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname);
+
+app
+
+//for syncing ClientApp
+.use( loopback.static(path.resolve(__dirname, '../client')) )
+
+.use( '/', function(req, res, next) {
+  res.sendFile('client/index.html', {root: '../'});
+});
 
 app.start = function() {
   // start the web server
