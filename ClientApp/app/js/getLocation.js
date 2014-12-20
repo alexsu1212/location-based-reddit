1607029cf7387786bad5a3b68ee674ece89a8879
yenming <Yenming@YenmingdeMacBook-Pro.local>
2014-12-20 20:11:33 +0800

    Initial commit

---
+++ ClientApp/app/js/getLocation.js
@@ -0,0 +1,8 @@
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
---
+++ ClientApp/app/js/showPosition.js
@@ -0,0 +1,5 @@
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    			"<br>Longitude: " + position.coords.longitude;	
+}/*showPosition*/
