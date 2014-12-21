9a7a2c425e233e148ae8d0c3653df9e4a6f473eb
Andrew <chenpoanandrew@gmail.com>
2014-12-21 13:19:18 +0800

    修正barInfo css

--- ClientApp/app/js/views/Header.jsx
+++ ClientApp/app/js/views/Header.jsx
@@ -12,14 +12,20 @@ var Header = React.createClass({
   render: function() {
 
     return (
-        <div className="bar">
-            <h3 className="webSiteTitle">Location Based Reddit</h3>
+        <div>
+            <div className="bar">
+                <h3 className="webSiteTitle">Location Based Reddit</h3>
 
-            <div className="barInfo">
-                <img className="userImg" src="assets/img/userHeadImg.jpg" alt="userImg" />
-                <h3 className="userName">陳柏安</h3>
-                <h5 className="signIn">log In</h5>
-                <h5 className="signUp">Sign Up</h5>
+                <div className="barInfo">
+                    <img className="userImg" src="assets/img/userHeadImg.jpg" alt="userImg" />
+                    <h3 className="userName">陳柏安</h3>
+                    <h5 className="signIn">log In</h5>
+                    <h5 className="signUp">Sign Up</h5>
+                </div>
+            </div>
+            <div className="header">
+                <h1 className="header-name"> Location Based Reddit</h1>
+                <p className="header-by">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;by Team PAMM</p>
             </div>
         </div>
 
@@ -27,10 +33,6 @@ var Header = React.createClass({
 
   },
 
-//        <div className="header">
-//            <h1 className="header-name"> Location Based Reddit</h1>
-//            <p className="header-by">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;by Team PAMM</p>
-//        </div>
   noop: function(){
 
   }
