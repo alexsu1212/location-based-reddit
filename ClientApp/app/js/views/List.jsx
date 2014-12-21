d5eabb83a84c0e384d8d23150f1153a2dddb9562
Andrew <chenpoanandrew@gmail.com>
2014-12-21 11:31:19 +0800

    move timer to list.jsx

--- ClientApp/app/js/views/List.jsx
+++ ClientApp/app/js/views/List.jsx
@@ -35,6 +35,41 @@ var comp = React.createClass({
         <table className="table table-hover">
             {arr}
         </table>
+        
+        <script>
+            var actions = require('../actions/ActionLib');
+
+            var timerTickLength = 1000 * 60 * 5; //1秒 X 60 X 分鐘
+            //var timerTickLength = 5000; //testing
+
+            $(document).ready(function () {
+                setTimeout("timer()", 0);
+            });
+
+            function timer() {
+                getLocation();
+                setTimeout("timer()", timerTickLength);
+            }
+
+            function getLocation() {
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(showPosition);
+                } else {
+                    alert("Geolocation is not supported by this browser.");
+                }
+            }
+
+            function showPosition(position) {
+            //    alert("Latitude: " + position.coords.latitude +
+            //        "\nLongitude: " + position.coords.longitude);
+
+                var posi = {
+                    position.coords.latitude, position.coords.longitude
+                };
+
+                actions.load(posi);
+            }
+        </script> 
     );
 
   },
