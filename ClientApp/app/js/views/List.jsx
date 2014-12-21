672bfb518e0229bbb58ba36be015398b6cd91a6b
Mickey <mickey@lt>
2014-12-21 14:16:55 +0800

    can dynamicly update list

--- ClientApp/app/js/views/List.jsx
+++ ClientApp/app/js/views/List.jsx
@@ -4,7 +4,7 @@
 
 var actions = require('../actions/ActionLib');
 var ListItem = React.createFactory(require('./ListItem.jsx'));
-var timerTickLength = 1000 * 60 * 5; //1秒 X 60 X 分鐘
+var timerTickLength = 1000 * 5 //1000 * 60 * 5; //1秒 X 60 X 分鐘
 //
 var comp = React.createClass({
 
@@ -12,6 +12,7 @@ var comp = React.createClass({
    *
    */
   render: function() {
+    this.timer();
 
     // 取出所有要繪製的資料
     var arrPins = this.props.truth.arrPins;
@@ -19,26 +20,23 @@ var comp = React.createClass({
     // 跑 loop 一筆筆建成 ListItem 元件
     var arr = arrPins.map(function(item){
 
-        // 注意每個 item 要有一個獨一無二的 key 值
-        return <ListItem
+      // 注意每個 item 要有一個獨一無二的 key 值
+      return <ListItem
 
-                todoItem={item}
-                selected={this.props.truth.selectedItem == item}
-                key={item.id}
+              todoItem={item}
+              selected={this.props.truth.selectedItem == item}
+              key={item.id}
 
-                onClick={this.handleClick.bind(this, item)}
-                onRemove={this.handleRemove} />
+              onClick={this.handleClick.bind(this, item)}
+              onRemove={this.handleRemove} />
 
     }, this)
 
 
     return (
-      <div>
-        <table className="table table-hover">
-            {arr}
-        </table>
-        <script></script>
-      </div>
+      <table className="table table-hover">
+          {arr}
+      </table>
     );
 
   },
@@ -47,15 +45,14 @@ var comp = React.createClass({
 //        <div className="contentList">
 
   timer: function() {
-    getLocation();
-    setTimeout("timer()", timerTickLength);
+    setTimeout(this.getLocation(), timerTickLength);
   },
 
   getLocation: function() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+      navigator.geolocation.getCurrentPosition(this.showPosition);
     } else {
-        alert("Geolocation is not supported by this browser.");
+      alert("Geolocation is not supported by this browser.");
     }
   },
 
@@ -63,7 +60,7 @@ var comp = React.createClass({
   //    alert("Latitude: " + position.coords.latitude +
   //        "\nLongitude: " + position.coords.longitude);
 
-    var posi = position.coords.latitude + ',' + position.coords.longitude;
+    var posi = position.coords.longitude + ',' + position.coords.latitude;
 
     actions.load(posi);
   },
