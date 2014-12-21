305f9d69a9cb158b6b4c09655657b9359ba7b272
Alex Su <alexpp44@gmail.com>
2014-12-21 16:21:20 +0800

    add a button to create pin with random data

--- ClientApp/app/js/views/Header.jsx
+++ ClientApp/app/js/views/Header.jsx
@@ -23,7 +23,9 @@ var Header = React.createClass({
                     <h5 className="signUp">Sign Up</h5>
                 </div>
             </div>
+
             <div className="header">
+                <button className="create-data btn btn-info" onClick={this.createData}>Create Data</button>
                 <h1 className="header-name"> Location Based Reddit</h1>
                 <p className="header-by">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;by Team PAMM</p>
             </div>
@@ -32,6 +34,59 @@ var Header = React.createClass({
     );
 
   },
+
+getRandomInt: function(start, end) {
+  return Math.floor(Math.random() * (end - start + 1) + start);
+},
+
+getRandomNum: function(start, end) {
+  return Math.random() * (end - start) + start;
+},
+
+createData: function(evt){
+      console.log( 'create new item' );
+
+      var dummyTitles = [
+        "Lorem ipsum dolor",
+        "Aenean commodo ligula",
+        "Cum sociis natoque",
+        "Donec quam felis",
+        "Nulla consequat massa",
+        "Donec pede justo",
+        "Integer tincidunt",
+        "Cras dapibus",
+        "Vivamus elementum semper",
+        "Aenean vulputate",
+        "Phasellus viverra nulla"
+      ];
+      var title = dummyTitles[this.getRandomInt(0, dummyTitles.length - 1)];
+
+      var longitude = this.getRandomNum(119, 121);
+      var altitude = this.getRandomNum(20, 22);
+
+      var createdAt = new Date();
+      createdAt.setHours(createdAt.getHours() - this.getRandomInt(0, 23));
+      
+      var upVotes = this.getRandomInt(4000 , 8000);
+      var downVotes = this.getRandomInt(0, 2000);
+
+      var item = {
+      "title": title,
+      "imgUrl": "",
+      "content": "",
+      "location": longitude.toString() + ", " + altitude.toString(),
+      "upVotes": upVotes,
+      "downVotes": downVotes,
+      "createdAt": createdAt
+      }
+
+      item.id = window.cnt++; //uuid.get();
+
+      actions.createPin( item );
+
+      // // 清空輸入框，等待下一次的輸入
+      // this.setState({currentItem: {name:'', id: null, created: null}});
+},
 
   noop: function(){
 
